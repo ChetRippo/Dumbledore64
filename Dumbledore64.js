6d6b89484e4a3785a75d28b0826cd0f32a6e6462
Brett Davis <bdavis1000@gmail.com>
2012-08-05 00:15:13 -0400

    Updated readme

--- Dumbledore64.js
+++ Dumbledore64.js
@@ -1,19 +1,4 @@
- /*        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-                 Version 2, December 2004
-
- Copyright (C) 2012 Brett Davis <bdavis1000@gmail.com>
-
- Everyone is permitted to copy and distribute verbatim or modified
- copies of this license document, and changing it is allowed as long
- as the name is changed.
-
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-
-  0. You just DO WHAT THE FUCK YOU WANT TO.
-
-*/
-/*
+ /*
 	Version 0.8.0 Changes(6/7/2012):
 		-Bug fixes:
 			-Fixed bug where after having a spell stolen fire, ice, dash, and bubble ring would not work
